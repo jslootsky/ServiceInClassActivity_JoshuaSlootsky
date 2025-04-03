5af4ee4386f9f093db845f36855da7ed9df51586
jslootsky <joshuaslootsky@gmail.com>
2025-04-03 12:11:21 -0400

    UI tweak - set text display to 0 on a stop

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -5,17 +5,29 @@ import android.content.Intent
 import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.os.IBinder
+import android.os.Looper
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var displayTextView: TextView
 
     var isConnected = false
     lateinit var timerBinder: TimerService.TimerBinder
 
+    val timerHandler = Handler(Looper.getMainLooper()){
+        displayTextView.text = it.what.toString()
+        true
+    }
+
     private val serviceConnection = object : ServiceConnection{
         override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
             timerBinder = service as TimerService.TimerBinder
+            timerBinder.setHandler(timerHandler)
             isConnected = true
         }
 
@@ -30,7 +42,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        displayTextView = findViewById(R.id.textView)
 
         bindService(
             Intent(this, TimerService::class.java),
@@ -47,6 +59,7 @@ class MainActivity : AppCompatActivity() {
         
         findViewById<Button>(R.id.stopButton).setOnClickListener {
             if(isConnected) timerBinder.stop()
+            displayTextView.text = "0"
         }
 
 
